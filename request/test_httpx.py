9dcc5fbc75610fc430aa5b8b0a394a57ccbde9d6
anton <a.solovjev@vzv.su>
2024-05-08 11:59:52 +0300

    testing with httpx_mock

--- request/test_httpx.py
+++ request/test_httpx.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_httpx import HTTPXMock
 
 from send_req import send_get, send_post
 
@@ -8,16 +9,22 @@ def httpbin_url():
     return "https://httpbin.org"
 
 
-def test_send_get(httpbin_url):
+def test_send_get(httpbin_url, httpx_mock: HTTPXMock):
     url = f"{httpbin_url}/get"
+    expected_response = {"url": url, "args": {}}
+
+    httpx_mock.add_response(url=url, json=expected_response)
     response = send_get(url)
-    assert response["url"] == url
-    assert response["args"] == {}
+
+    assert response == expected_response
 
 
-def test_send_post(httpbin_url):
+def test_send_post(httpbin_url, httpx_mock: HTTPXMock):
     url = f"{httpbin_url}/post"
     post_data = {'integer': 123, 'boolean': True, 'list': ['a', 'b', 'c']}
+    expected_response = {"url": url, "json": post_data}
+
+    httpx_mock.add_response(url=url, json=expected_response)
     response = send_post(url, post_data)
-    assert response["url"] == url
-    assert response["json"] == post_data
+
+    assert response == expected_response
